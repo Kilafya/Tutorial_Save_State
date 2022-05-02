962c9bb6590cc23fc8d48ac6e4c7613b689cb3f7
drayl <sunrise.161@mail.ru>
2022-05-02 21:33:28 +0300

    Added MainActivity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.myapplication
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import com.example.myapplication.databinding.ActivityMainBinding
+import kotlin.random.Random
+import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+private lateinit var mViewBinding: ActivityMainBinding
+
+   override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       mViewBinding = ActivityMainBinding.inflate(layoutInflater)
+       with (mViewBinding) {
+           setContentView(root)
+           btnIncrement.setOnClickListener { increment() }
+           btnColor.setOnClickListener { setRandomColor() }
+           btnVisibility.setOnClickListener { setVisibility() }
+       }
+    }
+
+    private fun increment() {
+        val value = mViewBinding.tv.text.toString().toInt()
+        mViewBinding.tv.text = (value + 1).toString()
+    }
+
+    private fun setRandomColor() {
+        val color = Color.rgb(
+            Random.nextInt(256),
+            Random.nextInt(256),
+            Random.nextInt(256)
+        )
+        mViewBinding.tv.setTextColor(color)
+    }
+
+    private fun setVisibility() {
+        with (mViewBinding) {
+            if (tv.visibility == View.VISIBLE) {
+                tv.visibility = View.INVISIBLE
+                btnVisibility.text = "Visible"
+            } else {
+                tv.visibility = View.VISIBLE
+                btnVisibility.text = "Invisible"
+            }
+        }
     }
 }
