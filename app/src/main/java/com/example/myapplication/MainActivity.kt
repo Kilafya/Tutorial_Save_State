c78cfc03bd397371a3ca30a8213e240193faa8ec
drayl <sunrise.161@mail.ru>
2022-05-03 17:01:11 +0300

    Added MainActivity2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,52 +1,58 @@
 package com.example.myapplication
 
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.PersistableBundle
-import com.example.myapplication.databinding.ActivityMainBinding
-import kotlin.random.Random
-import android.view.View
+    import android.content.Intent
+    import android.graphics.Color
+    import androidx.appcompat.app.AppCompatActivity
+    import android.os.Bundle
+    import com.example.myapplication.databinding.ActivityMainBinding
+    import kotlin.random.Random
+    import android.view.View
 
 
-class MainActivity : AppCompatActivity() {
+    class MainActivity : AppCompatActivity() {
 
-private lateinit var mViewBinding: ActivityMainBinding
+    private lateinit var mViewBinding: ActivityMainBinding
 
-   override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-       mViewBinding = ActivityMainBinding.inflate(layoutInflater)
-       with (mViewBinding) {
-           setContentView(root)
-           btnIncrement.setOnClickListener { increment() }
-           btnColor.setOnClickListener { setRandomColor() }
-           btnVisibility.setOnClickListener { setVisibility() }
-       }
-    }
+       override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+           mViewBinding = ActivityMainBinding.inflate(layoutInflater)
+           with (mViewBinding) {
+               setContentView(root)
+               btnIncrement.setOnClickListener { increment() }
+               btnColor.setOnClickListener { setRandomColor() }
+               btnVisibility.setOnClickListener { setVisibility() }
+               btnNext.setOnClickListener { goNextActivity() }
+           }
+        }
 
-    private fun increment() {
-        val value = mViewBinding.tv.text.toString().toInt()
-        mViewBinding.tv.text = (value + 1).toString()
-    }
+        private fun increment() {
+            val value = mViewBinding.tv.text.toString().toInt()
+            mViewBinding.tv.text = (value + 1).toString()
+        }
 
-    private fun setRandomColor() {
-        val color = Color.rgb(
-            Random.nextInt(256),
-            Random.nextInt(256),
-            Random.nextInt(256)
-        )
-        mViewBinding.tv.setTextColor(color)
-    }
+        private fun setRandomColor() {
+            val color = Color.rgb(
+                Random.nextInt(256),
+                Random.nextInt(256),
+                Random.nextInt(256)
+            )
+            mViewBinding.tv.setTextColor(color)
+        }
 
-    private fun setVisibility() {
-        with (mViewBinding) {
-            if (tv.visibility == View.VISIBLE) {
-                tv.visibility = View.INVISIBLE
-                btnVisibility.text = "Visible"
-            } else {
-                tv.visibility = View.VISIBLE
-                btnVisibility.text = "Invisible"
+        private fun setVisibility() {
+            with (mViewBinding) {
+                if (tv.visibility == View.VISIBLE) {
+                    tv.visibility = View.INVISIBLE
+                    btnVisibility.text = "Visible"
+                } else {
+                    tv.visibility = View.VISIBLE
+                    btnVisibility.text = "Invisible"
+                }
             }
         }
+
+        private fun goNextActivity() {
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
     }
-}
